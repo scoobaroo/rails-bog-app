7020436cd1acf7cb6ca66a9dd1dc42958ea4fae6
scoobaroo <scoobaroo415@gmail.com>
2016-04-29 14:16:57 -0700

    working DELETE

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -37,4 +37,18 @@ class CreaturesController < ApplicationController
     creature.update_attributes(creature_params)
     redirect_to creature_path(creature)
   end
+  def destroy
+    # get the creature id from the url params
+    creature_id = params[:id]
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    creature = Creature.find_by_id(creature_id)
+    # destroy the creature
+    creature.destroy
+    # redirect to creatures index
+    redirect_to creatures_path
+    # redirect_to creatures_path is equivalent to:
+    # redirect_to "/creatures"
+  end
+
 end
